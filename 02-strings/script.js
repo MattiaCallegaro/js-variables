0d271b7d9d49a41f9551316432491cf604c87715
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-15 19:39:12 +0100

    [Done] Fix exercise 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,14 +1,14 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-let name = 'Mattia';
-console.log(name);
+let myname = 'Mattia';
+console.log(myname);
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 let number = '10';
-let text = 'ciao come stai';
+let text = 'ciao come stai';qq
 let bool = 'true';
 console.log(number+" "+text+" "+bool);
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
